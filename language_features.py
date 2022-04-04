b01d3d183350b6c2c50bdbd70b1ca6d0ec1508f8
Vignesh Muthukumar <vickymhs@gmail.com>
2022-04-04 12:43:23 -0400

    Added sentence length and person name computation

---
+++ language_features.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import spacy
+from spacy import displacy
+
+
+def read_from_file(file_name):
+    print("Importing csv")
+    processed_data = pd.read_csv(file_name)
+    return processed_data
+
+
+def compute_question_length(input_data):
+    questions = input_data['Question']
+    question_length = []
+    for i in range(0, questions.shape[0]):
+        question_length.append(len(questions[i]))
+    return question_length
+
+
+def named_entity_recognition(input_data, entity_label_type):
+    questions = input_data['Question']
+    named_entities = []
+    # question = questions[1]
+    # doc = nlp(question)
+    # print(doc)
+    # for ent in doc.ents:
+    #     print(ent.text, " ", ent.label_)
+    # displacy.serve(doc, style="ent")
+
+    for question in questions:
+        names = []
+        doc = nlp(question)
+        for ent in doc.ents:
+            if ent.label_ == entity_label_type:
+                names.append(ent.text)
+        named_entities.append(names)
+
+    return named_entities
+    #     displacy.serve(doc, style="ent")
+
+
+def get_name_count(names):
+    name_count = []
+    name_boolean = []
+    for name in names:
+        name_count.append(len(name))
+        if len(name) > 0:
+            name_boolean.append(True)
+        else:
+            name_boolean.append(False)
+    return name_count, name_boolean
+
+
+def get_person_name_count_details(input_data):
+    person_names = named_entity_recognition(input_data, "PERSON")
+    name_count, name_boolean = get_name_count(person_names)
+    print(person_name_count)
+    print(person_name_boolean)
+    return name_count, name_boolean
+
+
+if __name__ == '__main__':
+    nlp = spacy.load("en_core_web_sm")
+    data = read_from_file("More_Processed_Data.csv")
+
+    # This will be the final dataframe
+    data_language_features = data.copy()
+
+    # Extract sentence length
+    ques_length = compute_question_length(data)
+
+    # Find all person names in a given sentence
+    person_name_count, person_name_boolean = get_person_name_count_details(data)
+
+    # Finally append all the relevant columns
+    data_language_features['question_length'] = np.array(ques_length)
+    print(data_language_features.head())
